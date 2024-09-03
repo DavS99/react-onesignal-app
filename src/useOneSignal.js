c7b5a6bbabc449e4b5485657e0e08bb94c5b63fb
DavS99 <davit@ucraft.com>
2024-09-03 19:04:10 +0400

    change key

--- src/useOneSignal.js
+++ src/useOneSignal.js
@@ -4,8 +4,8 @@ import OneSignal from 'react-onesignal';
 const useInitOneSignal = () => {
   useEffect(() => {
       OneSignal.init({
-        appId: "b15fe36e-bbf9-452b-a738-2761ff21333d",
-        apiKey: "Y2FlZjU2ZGYtM2I4Ni00YjYyLTliYzUtNWI2NGYyYzdhYWNm",
+        appId: "66c2daed-9dfa-4c9b-9d2b-aba613424cef",
+        apiKey: "ZTBhNTQ0ZjQtMmQ5MS00NTE5LTg4YjAtNDA1MzdiNzdhNmY2",
         allowLocalhostAsSecureOrigin: true
       }).then(() => {
         console.log('Init OneSignal!');
@@ -14,3 +14,6 @@ const useInitOneSignal = () => {
 };
 
 export default useInitOneSignal;
+
+
+
