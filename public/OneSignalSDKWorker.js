694d7ec630968ab9c93089397d63b1e9770fec11
DavS99 <davit@ucraft.com>
2024-09-03 19:24:11 +0400

    change key

--- public/OneSignalSDKWorker.js
+++ public/OneSignalSDKWorker.js
@@ -1,6 +1,5 @@
 if ('function' === typeof importScripts) {
-  importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
-
+  importScripts("https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js");
   function onMessage(e) {
     console.log(e);
     // do some work here
