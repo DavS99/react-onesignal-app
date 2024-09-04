9ef8f7bd85598440d3fd53e9215fabbb47967502
DavS99 <davit@ucraft.com>
2024-09-04 14:30:29 +0400

    c

--- src/useOneSignal.js
+++ src/useOneSignal.js
@@ -8,7 +8,23 @@ const useInitOneSignal = () => {
       apiKey: "ZTBhNTQ0ZjQtMmQ5MS00NTE5LTg4YjAtNDA1MzdiNzdhNmY2",
       allowLocalhostAsSecureOrigin: true,
     }).then(() => {
-      console.log("Init OneSignal!");
+      const timer = setTimeout(() => {
+        if (
+          OneSignal &&
+          OneSignal.Notifications &&
+          OneSignal.Notifications.requestPermission
+        ) {
+          OneSignal.Notifications.requestPermission()
+            .then((permission) => {
+              console.log("Notification permission status:", permission);
+            })
+            .catch((error) => {
+              console.error("Permission request failed:", error);
+            });
+        }
+      }, 1000); // 1-second delay to ensure page load is complete
+
+      return () => clearTimeout(timer);
     });
   }, []);
 };
