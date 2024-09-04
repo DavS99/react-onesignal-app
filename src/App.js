dc7847c2260c96c569d4dce4ec852b553c1ae13e
DavS99 <davit@ucraft.com>
2024-09-04 11:59:47 +0400

    xw

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import useInitOneSignal from "./useOneSignal";
+import OneSignal from "react-onesignal";
 
 function App() {
   useInitOneSignal();
+  const requestPermission = async () => {
+    try {
+      await OneSignal.Notifications.requestPermission();
+      console.log("request permission on");
+    } catch (error) {
+      console.error("Failed to request permission:", error);
+    }
+  };
 
   return (
     <div className="App">
       <h2>Click</h2>
-      <button>Trigger prompt</button>
+      <button onClick={requestPermission}>Trigger prompt</button>
+      <div onClick={requestPermission}>div click</div>
     </div>
   );
 }
