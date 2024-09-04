0bb64f6280217a2b12200f7c367c726d25cb0a98
DavS99 <davit@ucraft.com>
2024-09-04 15:07:30 +0400

    c

--- src/useOneSignal.js
+++ src/useOneSignal.js
@@ -3,8 +3,7 @@ import OneSignal from "react-onesignal";
 
 const useInitOneSignal = () => {
   useEffect(() => {
-    const requestPermission = () => {
-      console.log("aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
+    const handleUserInteraction = () => {
       if (
         OneSignal &&
         OneSignal.Notifications &&
@@ -18,17 +17,27 @@ const useInitOneSignal = () => {
             console.error("Permission request failed:", error);
           });
       }
+
+      // Remove event listener after triggering the permission request
+      window.removeEventListener("click", handleUserInteraction);
+      window.removeEventListener("scroll", handleUserInteraction);
     };
 
     OneSignal.init({
       appId: "66c2daed-9dfa-4c9b-9d2b-aba613424cef",
       apiKey: "ZTBhNTQ0ZjQtMmQ5MS00NTE5LTg4YjAtNDA1MzdiNzdhNmY2",
       allowLocalhostAsSecureOrigin: true,
-    }).then(() => {});
+    }).then(() => {
+      console.log("init");
+      // Attach event listeners for click or scroll
+      window.addEventListener("click", handleUserInteraction);
+      window.addEventListener("scroll", handleUserInteraction);
+    });
 
-    window.addEventListener("load", requestPermission);
-
-    return () => window.removeEventListener("load", requestPermission);
+    return () => {
+      window.removeEventListener("click", handleUserInteraction);
+      window.removeEventListener("scroll", handleUserInteraction);
+    };
   }, []);
 };
 
