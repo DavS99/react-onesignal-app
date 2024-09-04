982e643021f6e2b8fde69bd121fb4ffad767b4f8
DavS99 <davit@ucraft.com>
2024-09-04 12:50:20 +0400

    xw

--- src/useOneSignal.js
+++ src/useOneSignal.js
@@ -3,21 +3,11 @@ import OneSignal from "react-onesignal";
 
 const useInitOneSignal = () => {
   useEffect(() => {
-    const requestPermission = async () => {
-      try {
-        await OneSignal.Notifications.requestPermission();
-        console.log("request permission on");
-      } catch (error) {
-        console.error("Failed to request permission:", error);
-      }
-    };
-
     OneSignal.init({
       appId: "66c2daed-9dfa-4c9b-9d2b-aba613424cef",
       apiKey: "ZTBhNTQ0ZjQtMmQ5MS00NTE5LTg4YjAtNDA1MzdiNzdhNmY2",
       allowLocalhostAsSecureOrigin: true,
     }).then(() => {
-      requestPermission();
       console.log("Init OneSignal!");
     });
   }, []);
