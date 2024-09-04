1a665a8a5155a8b2a8982754aabfce1a18fab014
DavS99 <davit@ucraft.com>
2024-09-04 14:39:13 +0400

    c

--- src/useOneSignal.js
+++ src/useOneSignal.js
@@ -3,28 +3,20 @@ import OneSignal from "react-onesignal";
 
 const useInitOneSignal = () => {
   useEffect(() => {
+    const requestPermission = async () => {
+      try {
+        await OneSignal.Notifications.requestPermission();
+      } catch (error) {
+        console.error("Failed to request permission:", error);
+      }
+    };
+
     OneSignal.init({
       appId: "66c2daed-9dfa-4c9b-9d2b-aba613424cef",
       apiKey: "ZTBhNTQ0ZjQtMmQ5MS00NTE5LTg4YjAtNDA1MzdiNzdhNmY2",
       allowLocalhostAsSecureOrigin: true,
     }).then(() => {
-      const timer = setTimeout(() => {
-        if (
-          OneSignal &&
-          OneSignal.Notifications &&
-          OneSignal.Notifications.requestPermission
-        ) {
-          OneSignal.Notifications.requestPermission()
-            .then((permission) => {
-              console.log("Notification permission status:", permission);
-            })
-            .catch((error) => {
-              console.error("Permission request failed:", error);
-            });
-        }
-      }, 1000); // 1-second delay to ensure page load is complete
-
-      return () => clearTimeout(timer);
+      window.onload = requestPermission;
     });
   }, []);
 };
